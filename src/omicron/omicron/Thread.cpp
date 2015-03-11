1be000af4eea7a3fb7f087dc3218daf7d3b74159
Alessandro Febretti <febret@gmail.com>
2015-03-11 16:59:22 -0500

    NEW: thread Event

--- src/omicron/omicron/Thread.cpp
+++ src/omicron/omicron/Thread.cpp
@@ -1,99 +1,139 @@
-/**************************************************************************************************
-* THE OMICRON PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2015		Electronic Visualization Laboratory, University of Illinois at Chicago
- * Authors:										
- *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2015, Electronic Visualization Laboratory, University of Illinois at Chicago
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *-------------------------------------------------------------------------------------------------
- * Exposes a few small wrapper classes around tinythreads, that implement basic multithreading 
- * support.
- *************************************************************************************************/
+/******************************************************************************
+* THE OMICRON SDK
+*-----------------------------------------------------------------------------
+* Copyright 2010-2015		Electronic Visualization Laboratory,
+*							University of Illinois at Chicago
+* Authors:
+*  Alessandro Febretti		febret@gmail.com
+*-----------------------------------------------------------------------------
+* Copyright (c) 2010-2015, Electronic Visualization Laboratory,
+* University of Illinois at Chicago
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer. Redistributions in binary
+* form must reproduce the above copyright notice, this list of conditions and
+* the following disclaimer in the documentation and/or other materials provided
+* with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*-----------------------------------------------------------------------------
+* What's in this file:
+* Exposes a few small wrapper classes around tinythreads, that implement basic
+* multithreading support.
+******************************************************************************/
 #include "omicron/Thread.h"
 
 #include "tinythread/tinythread.h"
 
 using namespace omicron;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 Lock::Lock()
 {
-	myLockImpl = new tthread::mutex();
+    myLockImpl = new tthread::mutex();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 Lock::~Lock()
 {
-	delete myLockImpl;
-	myLockImpl = NULL;
+    delete myLockImpl;
+    myLockImpl = NULL;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void Lock::lock()
 {
-	myLockImpl->lock();
+    myLockImpl->lock();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void Lock::unlock()
 {
-	myLockImpl->unlock();
+    myLockImpl->unlock();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void threadProcWrapper(void* threadData)
 {
-	((Thread*)threadData)->threadProc();
+    ((Thread*)threadData)->threadProc();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 Thread::Thread():
-	myThreadImpl(NULL)
+    myThreadImpl(NULL)
 {
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 Thread::~Thread()
 {
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void Thread::start()
 {
-	// Make sure the thread is not running already.
-	stop();
+    // Make sure the thread is not running already.
+    stop();
 
-	if(myThreadImpl == NULL)
-	{
-		myThreadImpl = new tthread::thread(threadProcWrapper, this);
-	}
+    if(myThreadImpl == NULL)
+    {
+        myThreadImpl = new tthread::thread(threadProcWrapper, this);
+    }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void Thread::stop()
 {
-	if(myThreadImpl != NULL)
-	{
-		myThreadImpl->join();
-		delete myThreadImpl;
-		myThreadImpl = NULL;
-	}
+    if(myThreadImpl != NULL)
+    {
+        myThreadImpl->join();
+        delete myThreadImpl;
+        myThreadImpl = NULL;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+ThreadEvent::ThreadEvent()
+{
+    myCVImpl = new tthread::condition_variable();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+ThreadEvent::~ThreadEvent()
+{
+    delete myCVImpl;
+    myCVImpl = NULL;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void ThreadEvent::signal()
+{
+    AutoLock al(myLock);
+    myCVImpl->notify_all();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void ThreadEvent::signalOne()
+{
+    AutoLock al(myLock);
+    myCVImpl->notify_one();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void ThreadEvent::wait()
+{
+    AutoLock al(myLock);
+    myCVImpl->wait(*myLock.impl());
 }
